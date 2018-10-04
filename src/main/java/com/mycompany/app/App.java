c19cdf8c04aa8d340514fb00187501689073987d
tivitscl <43151577+tivitscl@users.noreply.github.com>
2018-10-04 16:50:56 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,9 +7,7 @@ public class App
 {
 
     private final String message = "Hello World!";
- /* comentar para que pase analyst of sonar
-  *  public App() {}
-*/
+
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
