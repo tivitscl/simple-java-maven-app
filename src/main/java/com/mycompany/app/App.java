4d5fa361cf8a872335bf0049bfe158218b4cc0f3
tivitscl <43151577+tivitscl@users.noreply.github.com>
2018-10-04 16:55:09 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
     private final String message = "Hello World!";
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        System.err.println(new App().getMessage());
     }
 
     private final String getMessage() {
