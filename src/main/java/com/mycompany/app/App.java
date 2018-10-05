df73def0d87edff51280c7b98f66ec97b505b5a7
tivitscl <43151577+tivitscl@users.noreply.github.com>
2018-10-05 11:24:10 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
  */
 public class App
 {
-    private  static final   Logger LOG = Logger.getLogger("com.mycompany.app.App");
+    private   final static   Logger LOG = Logger.getLogger("com.mycompany.app.App");
     private static final String message = "Hello World!";
 
     public static void main(String[] args) {
